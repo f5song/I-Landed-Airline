d29d3b901b0456bdb2654656514c35ee8e863d33
stewy <118096987+sStoowie@users.noreply.github.com>
2023-10-18 15:26:35 +0700

    BAGGAGEจิ

---
+++ PAGE/BAGGAGE/baggage.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./baggage.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Noto Sans Thai:wght@500;600&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap"
+    />
+  </head>
+  <body>
+    <div class="baggage">
+      <div class="mix-group">
+        <div class="select-weight-baggage">
+          <div class="select-weight-baggage-child"></div>
+          <div class="logo">
+            <div class="i-landed-airline">I Landed Airline</div>
+            <img
+              class="logo-2-icon"
+              alt=""
+              src="./public/4137363-logo-2@2x.png"
+            />
+          </div>
+          <div class="text-departure-arrival">
+            Hua Hin (HHQ) → Chiang Mai (CNX)
+          </div>
+          <div class="line-top-frame-for-select-weig"></div>
+          <div class="frame-for-select-weight"></div>
+          <div class="text-total-baggage">สัมภาระทั้งหมด</div>
+          <div class="text-total-kg">0 กิโลกรัม</div>
+          <div class="button-baggage-0-kg" id="baggage-0kg">
+            <div class="icon-suitcase-rolling">
+              <div class="icon-suitcase-rolling-child"></div>
+              <img class="vector-icon" alt="" src="./public/vector.svg" />
+            </div>
+            <div class="div">ไม่มีสัมภาระเพิ่มเติม</div>
+            <div class="button-baggage-0-kg-child" id="button-baggage-0kg"></div>
+            <div class="text-price">0 บาท</div>
+          </div>
+          <div class="button-baggage-5-kg" id="baggage-5kg">
+            <div class="icon-suitcase-rolling">
+              <div class="icon-suitcase-rolling-child"></div>
+              <img class="vector-icon" alt="" src="./public/vector.svg" />
+            </div>
+            <div class="div">+ 5 กิโลกรัม</div>
+            <div class="button-baggage-0-kg-child" id="button-baggage-5kg"></div>
+            <div class="text-price">250 บาท</div>
+          </div>
+          <div class="button-baggage-10-kg" id="baggage-10kg">
+            <div class="icon-suitcase-rolling">
+              <div class="icon-suitcase-rolling-child"></div>
+              <img class="vector-icon" alt="" src="./public/vector.svg" />
+            </div>
+            <div class="div">+10 กิโลกรัม</div>
+            <div class="button-baggage-0-kg-child" id="button-baggage-10kg"></div>
+            <div class="text-price2">320 บาท</div>
+          </div>
+          <div class="button-baggage-15-kg" id="baggage-15kg">
+            <div class="icon-suitcase-rolling">
+              <div class="icon-suitcase-rolling-child"></div>
+              <img class="vector-icon" alt="" src="./public/vector.svg" />
+            </div>
+            <div class="div">+ 15 กิโลกรัม</div>
+            <div class="button-baggage-0-kg-child" id="button-baggage-15kg"></div>
+            <div class="text-price2">425 บาท</div>
+          </div>
+          <div class="button-baggage-20-kg" id="baggage-20kg">
+            <div class="icon-suitcase-rolling">
+              <div class="icon-suitcase-rolling-child"></div>
+              <img class="vector-icon" alt="" src="./public/vector.svg" />
+            </div>
+            <div class="div">+ 20 กิโลกรัม</div>
+            <div class="button-baggage-0-kg-child" id="button-baggage-20kg"></div>
+            <div class="text-price4">455 บาท</div>
+          </div>
+          <div class="text-name-passenger">Mrs.Panalee Chuckseeda</div>
+        </div>
+        <div class="baggage1">
+          <img class="icon-suitcase" alt="" src="./public/icon-suitcase.svg" />
+
+          <div class="text-baggage">สัมภาระ</div>
+          <div class="text-select-your">เลือกเที่ยวบินของคุณ</div>
+          <div class="text-select-more">เลือกสัมภาระเพิ่มเติมของคุณ</div>
+        </div>
+        <div class="choose-flight">
+          <div class="bg-departure-to-arrival"></div>
+          <div class="text-departure-to">Hua Hin (HHQ) → Chiang Mai (CNX)</div>
+          <div class="text-0kg-and-container">
+            <span class="text-0kg-and-container1">
+              <span>0 kg</span>
+              <span class="span"> </span>
+              <span>THB 1000.00</span>
+            </span>
+          </div>
+        </div>
+        <div class="total-price">
+          <div class="frame-for-total-price"></div>
+          <div class="text-price5">THB 1000.00</div>
+          <div class="text-description">
+            (สำหรับทุกเที่ยวบินและทุกผู้โดยสาร)
+          </div>
+          <div class="text-total-price">ราคาทั้งหมด</div>
+          <div id="continueButton" class="animation-continue btn-con">
+            <div class="text-continue">ดำเนินการต่อ</div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar">
+        <div class="navbar1"></div>
+        <img class="img-logo-icon" alt="" src="./public/img-logo@2x.png" />
+
+        <div class="process-three-step">
+          <div class="received-ticket">
+            <div class="text-received-ticket">รับตั๋ว</div>
+            <div class="num3">
+              <div class="pic-circle"></div>
+              <div class="text-3">3</div>
+            </div>
+          </div>
+          <img
+            class="separate-line-icon"
+            alt=""
+            src="./public/separate-line.svg"
+          />
+
+          <div class="paid">
+            <div class="text-received-ticket">ชำระเงิน</div>
+            <div class="num2">
+              <div class="pic-circle"></div>
+              <div class="text-3">2</div>
+            </div>
+          </div>
+          <img
+            class="seperate-line-icon"
+            alt=""
+            src="./public/separate-line.svg"
+          />
+
+          <div class="order">
+            <div class="text-received-ticket">จอง</div>
+            <div class="num1">
+              <div class="circle-1"></div>
+              <div class="text-3">1</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footber-bg"></div>
+      <div class="line-bottom-page"></div>
+      <img class="logo-3-icon" alt="" src="../homepage/public/4137364-logo-3@2x.png" />
+
+      <div class="text-and-button">
+        <img
+          class="icon-instagram"
+          alt=""
+          src="../homepage/public/vector.svg"
+        />
+
+        <img class="icon-envelope" alt="" src="../homepage/public/vector1.svg" />
+
+        <b class="text-contact-us">ติดต่อเรา</b>
+        <div class="button-ani-sign-up">
+          <div class="text-sign-up-container">
+            <p class="p">ลงทะเบียน</p>
+          </div>
+        </div>
+        <div class="button-ani-login">
+          <div class="text-sign-up-container">เข้าสู่ระบบ</div>
+        </div>
+        <b class="text-account">ACCOUNT</b>
+        <div class="ani-help">
+          <div class="p">ช่วยเหลือ</div>
+        </div>
+        <div class="ani-homepage">
+          <div class="text-homepage">
+            <p class="p">หน้าแรก</p>
+          </div>
+        </div>
+        <div class="ani-recomm-place">
+          <div class="text-recomm-footer">
+            <p class="p">แนะนำสถานที่</p>
+          </div>
+        </div>
+        <b class="text-ilanded-airline">I-LANDED AIRLINE</b>
+      </div>
+    </div>
+  </body>
+  <script>
+
+    // สร้างตัวแปรสำหรับเก็บ Object ที่กำลังเลือก
+    let selectedObject = null;
+
+    // เลือกทั้งหมดของ Object และเก็บไว้ในตัวแปร
+    const baggage0kg = document.getElementById("baggage-0kg");
+    const button_baggage0kg = document.getElementById("button-baggage-0kg");
+    const baggage5kg = document.getElementById("baggage-5kg");
+    const button_baggage5kg = document.getElementById("button-baggage-5kg");
+    const baggage10kg = document.getElementById("baggage-10kg");
+    const button_baggage10kg = document.getElementById("button-baggage-10kg");
+    const baggage15kg = document.getElementById("baggage-15kg");
+    const button_baggage15kg = document.getElementById("button-baggage-15kg");
+    const baggage20kg = document.getElementById("baggage-20kg");
+    const button_baggage20kg = document.getElementById("button-baggage-20kg");
+    const nextButton = document.getElementById("continueButton");
+
+    // สร้างฟังก์ชันสำหรับการเปลี่ยนสีของ Object
+    function changeColor1(element) {
+      element.style.backgroundColor = "rgba(0,0,0, 0.5)";
+    }
+
+    // เพิ่มการฟังก์ชันในการคลิก Object แต่ละอัน
+    baggage0kg.addEventListener("click", function() {
+    if (selectedObject) {
+      selectedObject.style.backgroundColor = "";
+    }
+    selectedObject = button_baggage0kg;
+    changeColor1(button_baggage0kg);
+    enableNextButton();
+    });
+
+    baggage5kg.addEventListener("click", function() {
+    if (selectedObject) {
+      selectedObject.style.backgroundColor = "";
+    }
+    selectedObject = button_baggage5kg;
+    changeColor1(button_baggage5kg);
+    enableNextButton();
+    });
+
+    baggage10kg.addEventListener("click", function() {
+    if (selectedObject) {
+      selectedObject.style.backgroundColor = "";
+    }
+    selectedObject = button_baggage10kg;
+    changeColor1(button_baggage10kg);
+    enableNextButton();
+    });
+
+    baggage15kg.addEventListener("click", function() {
+    if (selectedObject) {
+      selectedObject.style.backgroundColor = "";
+    }
+    selectedObject = button_baggage15kg;
+    changeColor1(button_baggage15kg);
+    enableNextButton();
+    });
+
+    baggage20kg.addEventListener("click", function() {
+    if (selectedObject) {
+      selectedObject.style.backgroundColor = "";
+    }
+    selectedObject = button_baggage20kg;
+    changeColor1(button_baggage20kg);
+
+    enableNextButton();
+    });
+
+    // ฟังก์ชันเพื่อเปิดใช้งานหรือปิดใช้งานปุ่ม "ไปหน้าต่อไป"
+    function enableNextButton() {
+      if (selectedObject) {
+        nextButton.removeAttribute("disabled");
+      } else {
+        nextButton.setAttribute("disabled", "true");
+      }
+    }
+
+    // กำหนดปุ่ม "ไปหน้าต่อไป" ให้ไม่สามารถคลิกได้ในเริ่มต้น
+    nextButton.setAttribute("disabled", "true");
+
+    // เมื่อคลิกปุ่ม "ไปหน้าต่อไป" ตรวจสอบว่ามี Object ที่ถูกเลือกแล้วหรือไม่
+    nextButton.addEventListener("click", function(event) {
+      if (!selectedObject) {
+        alert("Please select a baggage.");
+        event.preventDefault(); // หยุดการทำงานของปุ่มถ้ายังไม่ได้เลือก Object
+      } else {
+        window.location.href = "../book3/book3.php";
+      }
+    });
+</script>
+</html>
