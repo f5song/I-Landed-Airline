ddea47f48aaca7744aefa6b105b0083648cf8b8e
PIRAPORN PRASERT <sasayaza@gmail.com>
2023-10-19 19:19:57 +0700

    ล็อคอิน สมัคร

---
+++ PAGE/SIGNUPLOGIN/login_real.php
@@ -0,0 +1,101 @@
+
+<!-- for go to browser -->
+
+<!-- http://localhost/ISAD-ilal/PAGE/SIGNUPLOGIN/login.php -->
+
+
+<?php
+    session_start();
+    require_once '../../CRUD/config/db.php';
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index_real.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto Sans Thai:wght@600&display=swap"/>
+  </head>
+  <body>
+  
+    <div class="real-login-page">
+      <img class="real-login-page-child" alt="" src="../SIGNUPLOGIN/public/ellipse-3.svg"/>
+      <img class="real-login-page-item" alt="" src="./public/group-4.svg" />
+      <img class="logo-3-icon" alt="" src="./public/4137363-logo-3@2x.png" />
+
+        <div class="group-parent">          
+          <div class="parent1">
+            <div class="div77">เข้าสู่ระบบ</div>
+            <div class="div78">จัดการการจองง่ายดาย พร้อมรับสิทธิประโยชน์เฉพาะสมาชิก</div>
+          </div>
+
+          <form id="login-form" action="login_db.php" method="POST">
+
+
+          <!-- P -->
+          <!-- H -->
+          <!-- P -->
+
+          <?php if(isset($_SESSION['error'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    ?>
+                </div>
+
+            <?php } ?>
+
+            <?php if(isset($_SESSION['success'])) { ?>
+                <div class="alert alert-success" role="alert">
+                    <?php
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                    ?>
+                </div>
+
+            <?php } ?>
+            
+            <?php if(isset($_SESSION['notify'])) { ?>
+                <div class="alert alert-warning" role="alert">
+                    <?php
+                        echo $_SESSION['notify'];
+                        unset($_SESSION['notify']);
+                    ?>
+                </div>
+
+            <?php } ?>
+
+            <div class="form-group1">
+              <label for="usernameEmail">Email</label>
+              <input type="text" id="usernameEmail" name="email" required>
+            </div>
+            <div class="form-group1">
+              <label for="password">Password</label>
+              <input type="password" id="password" name="password" required>
+            </div>
+            <div class="submit-form1">
+              <button class="logintohomepage" type="submit" id="submitbutton1" name="signin">เข้าสู่ระบบ</button>
+            </div>
+          </form>
+          <div class="goto-sing-in">ยังไม่มีบัญชีใช่หรือไม่? <a href="register.php" style="color: #003a6c;">สมัครเลย!</a></div>
+        </div>
+    </div>
+
+    
+    <script>
+      var usernameEmailText = document.getElementById("usernameEmailText");
+      if (usernameEmailText) {
+        usernameEmailText.addEventListener("click", function () {
+          var anchor = document.querySelector("[data-scroll-to='rectangle']");
+          if (anchor) {
+            anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+          }
+        });
+      }
+      </script>
+  </body>
+</html>
--- PAGE/SIGNUPLOGIN/register.php
+++ PAGE/SIGNUPLOGIN/register.php
@@ -1,97 +1,79 @@
-
+<!-- http://localhost/ISAD-ilal/PAGE/SIGNUPLOGIN/register.php -->
 
 
 <?php
-    session_start();
-    require_once '../../CRUD/config/db.php';
+session_start();
+require_once '../../CRUD/config/db.php';
 ?>
-
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
 
-    <link rel="stylesheet" href="./global.css" />
-    <link rel="stylesheet" href="./index.css" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Noto Sans Thai:wght@600&display=swap"
-    />
-  </head>
-  <body>
-    <div class="real-sign-up-page">
-      <img
-        class="real-sign-up-page-child"
-        alt=""
-        src="./public/ellipse-3.svg"
-      />
-      <img class="real-sign-up-page-item" alt="" src="./public/group-4.svg" />
-      <img class="logo-3-icon" alt="" src="./public/4137363-logo-3@2x.png" />
-      <div class="frame-div">
-        <form id="register-form" action="register_db.php" method="POST">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="initial-scale=1, width=device-width" />
 
+  <link rel="stylesheet" href="./index.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto Sans Thai:wght@600&display=swap" />
 
-        <!-- http://localhost/ISAD-ilal/PAGE/SIGNUPLOGIN//register.php -->
-          
-          <!-- P -->
-          <!-- H -->
-          <!-- P -->
-          
-          <?php if(isset($_SESSION['error'])) { ?>
-            <div class="alert-danger" role="alert">
-                <?php
-                    echo $_SESSION['error'];
-                    unset($_SESSION['error']);
-                ?>
-            </div>
-        <?php } ?>
+</head>
 
-        <?php if(isset($_SESSION['success'])) { ?>
-            <div class="alert-success" role="alert">
-                <?php
-                    echo $_SESSION['success'];
-                    unset($_SESSION['success']);
-                ?>
-            </div>
-        <?php } ?>
-
-        <?php if(isset($_SESSION['warning'])) { ?>
-            <div class="alert-warning" role="alert">
-                <?php
-                    echo $_SESSION['warning'];
-                    unset($_SESSION['warning']);
-                ?>
-            </div>
-        <?php } ?>
-
-        <!-- component -->
-          <div class="div">สมัครสมาชิก/สร้างบัญชี</div>
-          <div class="div1">
-            จัดการการจองง่ายดาย พร้อมรับสิทธิประโยชน์เฉพาะสมาชิก
-          </div>
-          <div class="form-group">
-            <label for="usernameEmail">Email</label>
-            <input type="text" id="usernameEmail" name="email" required>
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-          </div>
-          <div class="form-group">
-            <label for="confirmPassword">Confirm Password</label>
-            <input type="password" id="confirmPassword" name="c_password" required>
-          </div>
-          <div class="submit-form">
-            <button type="submit" id="submitbutton" name="signup" href="https://fonts.googleapis.com/css2?family=Noto Sans Thai:wght@600&display=swap">ลงทะเบียน</button>
-          </div>
-        </form>
+<body>
+  <div class="backgroundlock">
+    <div class="left">
+      <img class="img1" src="./img/cloud1.svg" alt="">
+      <img src="./img/logo_airline.png" alt="">
+      <img class="img2" src="./img/cloud2.svg" alt="">
     </div>
+    <div class="right">
+      <form id="register-form" action="register_db.php" method="POST">
+        <?php if (isset($_SESSION['error'])) { ?>
+          <div class="alert-danger" role="alert">
+            <?php
+            echo $_SESSION['error'];
+            unset($_SESSION['error']);
+            ?>
+          </div>
+        <?php } ?>
 
+        <?php if (isset($_SESSION['success'])) { ?>
+          <div class="alert-success" role="alert">
+            <?php
+            echo $_SESSION['success'];
+            unset($_SESSION['success']);
+            ?>
+          </div>
+        <?php } ?>
 
-    <!-- หาลิ้งค์ด้วยอีควาย ไปหน้า HOME ตรงbutton  -->
+        <?php if (isset($_SESSION['warning'])) { ?>
+          <div class="alert-warning" role="alert">
+            <?php
+            echo $_SESSION['warning'];
+            unset($_SESSION['warning']);
+            ?>
+          </div>
+        <?php } ?>
+        <p class="title">สมัครสมาชิก/สร้างบัญชี</p>
+        <p class="message">จัดการการจองง่ายดาย พร้อมรับสิทธิประโยชน์เฉพาะสมาชิก</p>
 
+        <label>
+          <input required placeholder="" type="email" class="input">
+          <span>Email</span>
+        </label>
 
-  </body>
+        <label>
+          <input required placeholder="" type="password" class="input">
+          <span>Password</span>
+        </label>
+        <label>
+          <input required placeholder="" type="password" class="input">
+          <span>Confirm password</span>
+        </label>
+        <button class="submit">ลงทะเบียน</button>
+        <p class="signin">มีบัญชีอยู่แล้วใช่หรือไม่? <a href="login.php">เข้าสู่ระบบ</a> </p>
+      </form>
+    </div>
+  </div>
+</body>
+
 </html>
